094383d6575fa9cab2b47dd6dd4a7dd0be037897
alam <shaikh.alam87@gmail.com>
2022-01-04 16:45:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       
-      <Router>
+      <Router basename='/shaikhalam87.github.io/react-image-navigate/'>
       <Header/>
         <Switch>
           <Route path='/' exact component={ProductList} />
